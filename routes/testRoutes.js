67ade2c484ef8de6d38b63d5a8f2d1768ac7de8d
kareli2005 <cotneqareli123@gmail.com>
2024-10-28 13:46:55 +0400

    testcommit 4

--- routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.router()
-const testController = require("../controllers/testController")
-
-router.get('/', testController.welcomeText)
-router.get('/home', testController.homeText)
-router.get('/login', testController.loginText)
-
-module.exports = router
